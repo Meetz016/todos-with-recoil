8f3252137f9edf78a0ae09cb60654184fdd757cf
meetz016 <kmeetz016@gmail.com>
2024-09-19 01:37:39 +0530

    cleaned css and added recoil atoms

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RecoilRoot, useRecoilState, useSetRecoilState } from "recoil"
+import { desAtom, titleAtom, todosAtom } from "./store/atoms/todos"
+import { useEffect } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <RecoilRoot>
+      <InputTodos></InputTodos>
+        </RecoilRoot>
+    </div>
   )
+}
+
+function InputTodos(){
+
+  const [title,setTitle]=useRecoilState(titleAtom)
+  const [des,setDes]=useRecoilState(desAtom)
+  const [todo,setTodo]=useRecoilState(todosAtom)
+
+  function handleSubmit(){
+    if(title && des){
+      console.log(title)
+      console.log(des)
+      setTodo((todo)=>[...todo,{title,des}])
+      setTitle("")
+      setDes("")
+    }
+  }
+
+  useEffect(()=>{
+    console.log(todo)
+  },[todo])
+
+  return <div>
+    <input type="text" placeholder="Enter Todo Title:" onChange={(e)=>setTitle(e.target.value)}></input>
+    <br/>
+    <br/>
+    <input type="text" placeholder="Enter Todo Description:" onChange={(e) =>setDes(e.target.value)}></input>
+    <br/>
+    <br/>
+    <button onClick={handleSubmit}>Submit Todo</button>
+  </div>
 }
 
 export default App
